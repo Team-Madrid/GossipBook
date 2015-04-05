a9a335a64a38dbb386e77f40a9e8632c77b9bae5
tr00per92 <zvezdo@mail.bg>
2015-04-05 11:16:38 +0300

    style fix

--- GossipBook.Services/Controllers/UserController.cs
+++ GossipBook.Services/Controllers/UserController.cs
@@ -1,9 +1,8 @@
-﻿using System.Linq;
-
-namespace GossipBook.Services.Controllers
+﻿namespace GossipBook.Services.Controllers
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Net.Http;
     using System.Security.Claims;
     using System.Security.Cryptography;
